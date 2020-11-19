4532a09bf8ca970963092f8771413a1d064036c8
Alex Danin <alexey.danin@gmail.com>
2020-11-19 15:58:59 +0100

    Modul 12.3

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './Hero.scss';
+
+//class Hero extends React.Component {
+//  render() {
+//    return (
+const Hero = () => (
+      <header className={styles.component}>
+        <h2 className={styles.title}>Things to do!!!</h2>
+        <img src='http://uploads.kodilla.com/bootcamp/fer/11.react/space.png' className={styles.image}/>
+      </header>
+    );
+//    )
+//  }
+//}
+
+export default Hero;

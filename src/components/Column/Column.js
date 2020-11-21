50a967f0b4de9c67024745867c0b61673f0703b5
Alex Danin <alexey.danin@gmail.com>
2020-11-21 14:56:00 +0100

    Modul 12.4

---
+++ src/components/Column/Column.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './Column.scss';
+import PropTypes from 'prop-types';
+import List from '../List/List.js';
+class Column extends React.Component {
+  static propTypes = {
+   nameColumsFromList: PropTypes.string,
+ }
+  render() {
+    return (
+      <section className={styles.component}>
+      <h3 className={styles.title}> {this.props.nameColumsFromList}</h3>
+      </section>
+    )
+  }
+}
+
+export default Column;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
+import PropTypes from 'prop-types';
+import Column from '../Column/Column.js';
 class List extends React.Component {
+  static propTypes = {
+   title: PropTypes.node,
+   children: PropTypes.node,
+  }
+   static defaultProps = {
+    children: <p>I can do all the things!!!</p>,
+   }
   render() {
     return (
       <section className={styles.component}>
-        <Hero />
-
+        <Hero titleText={this.props.title} imageHero={this.props.imageHerofromApp}/>
+        <div className={styles.description}>
+         {this.props.children}
+        </div>
+        <div className={styles.columns}>
+         <Column nameColumsFromList='Animals'/>
+         <Column nameColumsFromList='Plants'/>
+         <Column nameColumsFromList='Minerals'/>
+        </div>
       </section>
     )
   }
